363dc897b8dd1ce33f3dcc96ea4e4f789c70310c
Jimmy Cai <jimmehcai@gmail.com>
2021-03-24 21:48:21 +0100

    No invocar putPiece manualmente al crear la pieza

--- src/main/java/logic/gameObjects/Player.java
+++ src/main/java/logic/gameObjects/Player.java
@@ -1,12 +1,11 @@
 package logic.gameObjects;
 
+import exceptions.OccupiedCellException;
 import logic.Board.Side;
 import logic.Cell;
 import logic.Color;
 
 import java.util.HashSet;
-
-import exceptions.OccupiedCellException;
 
 public class Player {
     private Color color; // Color asignado al jugador
@@ -19,28 +18,27 @@ public class Player {
     //Constructor para debug exclusivamente
     public Player() throws OccupiedCellException {
         color = Color.Blue;
-        playerSide=Side.Down;
+        playerSide = Side.Down;
         createPieces();
     }
 
     public Player(Color color, Side start) throws OccupiedCellException {
         this.color = color;
-        playerSide=start;
+        playerSide = start;
         createPieces();
     }
-    
+
     /**
      * Genera las fichas de Player, solo se llama una vez al principio de la partida
-     * 
+     *
      * @throws OccupiedCellException si la zona en la que empieza esta ocupada
      */
     private void createPieces() throws OccupiedCellException {
-    	HashSet<Cell> in=this.playerSide.getSideCells();
-    	for(Cell i: in) {
-    		Piece aux=new Piece(i,this.color);
-    		pieces.add(aux);
-    		i.putPiece(aux);
-    	}
+        HashSet<Cell> in = this.playerSide.getSideCells();
+        for (Cell i : in) {
+            Piece aux = new Piece(i, this.color);
+            pieces.add(aux);
+        }
     }
 
     /* Getters */
