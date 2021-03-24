1104411f6d3ee5a0bdcf12ce57cda44d44b97831
pablo.cageaoh@gmail.com <pcageao@ucm.es>
2021-03-24 19:37:58 +0100

    Constructor clase Player

--- src/main/java/logic/gameObjects/Player.java
+++ src/main/java/logic/gameObjects/Player.java
@@ -1,9 +1,12 @@
 package logic.gameObjects;
 
 import logic.Board.Side;
+import logic.Cell;
 import logic.Color;
 
 import java.util.HashSet;
+
+import exceptions.OccupiedCellException;
 
 public class Player {
     private Color color; // Color asignado al jugador
@@ -13,12 +16,31 @@ public class Player {
 
     /*Constructores*/
 
-    public Player() {
+    //Constructor para debug exclusivamente
+    public Player() throws OccupiedCellException {
         color = Color.Blue;
+        playerSide=Side.Down;
+        createPieces();
     }
 
-    public Player(Color color) {
+    public Player(Color color, Side start) throws OccupiedCellException {
         this.color = color;
+        playerSide=start;
+        createPieces();
+    }
+    
+    /**
+     * Genera las fichas de Player, solo se llama una vez al principio de la partida
+     * 
+     * @throws OccupiedCellException si la zona en la que empieza esta ocupada
+     */
+    private void createPieces() throws OccupiedCellException {
+    	HashSet<Cell> in=this.playerSide.getSideCells();
+    	for(Cell i: in) {
+    		Piece aux=new Piece(i,this.color);
+    		pieces.add(aux);
+    		i.putPiece(aux);
+    	}
     }
 
     /* Getters */
