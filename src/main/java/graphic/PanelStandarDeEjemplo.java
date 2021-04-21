3ca69a21d50e301838c40b8bcb5f467949c06dd5
ayhon <ayhonnick@gmail.com>
2021-04-21 18:05:22 +0200

    Modificaciones a GameObserver

--- src/main/java/graphic/PanelStandarDeEjemplo.java
+++ src/main/java/graphic/PanelStandarDeEjemplo.java
@@ -17,40 +17,4 @@ public class PanelStandarDeEjemplo extends JPanel implements GameObserver {
         // TODO montar el componente
 
     }
-
-    @Override
-    public void onRegister(Game game) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onEndTurn(Game game) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onSurrendered(Game game) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onReset(Game game) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onGameEnded(Game game) {
-        // TODO Auto-generated method stub
-
-    }
-
-	@Override
-	public void onSelectedPiece(Piece piece) {
-		// TODO Auto-generated method stub
-		
-	}
 }
