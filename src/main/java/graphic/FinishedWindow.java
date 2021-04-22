ebb831fd835d9f0b24d397d6d75a89ac25c8023e
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2021-04-22 19:41:19 +0200

    Creacion de la clase de la ventana de salida

---
+++ src/main/java/graphic/FinishedWindow.java
@@ -0,0 +1,19 @@
+package graphic;
+
+import javax.swing.JFrame;
+
+
+
+public class FinishedWindow extends JFrame{
+    MainWindow father;
+    
+    
+    public FinishedWindow(MainWindow father) {
+	this.father = father;
+	initGUI();
+    }
+    
+    public void initGUI() {
+	
+    }
+}
