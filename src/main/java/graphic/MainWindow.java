c7e3efba7f427018ba620a1f56eb418980f09958
Jimmy <jimmehcai@gmail.com>
2021-04-22 20:02:26 +0200

    style: formatear los códigos

--- src/main/java/graphic/MainWindow.java
+++ src/main/java/graphic/MainWindow.java
@@ -5,12 +5,10 @@ import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import logic.Board;
 import logic.Game;
 import logic.gameObjects.Piece;
@@ -19,7 +17,7 @@ import logic.gameObjects.Player;
 //Se encargara de la interfaz dejando toda la logica interna a interacciones con ctrl
 public class MainWindow extends JFrame implements GameObserver {
     private Controller ctrl;
-    
+
     //Pantallas principales
     private JPanel startScreen;
     private JPanel gameScreen;
@@ -35,31 +33,31 @@ public class MainWindow extends JFrame implements GameObserver {
     }
 
     private void initGUI() {
-	initStart();
-	initGameOptions();
-	this.setContentPane(startScreen);
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.pack();
-	this.setVisible(true);
+        initStart();
+        initGameOptions();
+        this.setContentPane(startScreen);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
     }
-    
+
     private void initStart() {
-	//TODO crear pantalla initScreen
+        //TODO crear pantalla initScreen
     }
-    
+
     private void initGame() {
-	//TODO crear pantalla gameScreen
+        //TODO crear pantalla gameScreen
     }
-    
+
     private void initWinner() {
-	//TODO crear pantalla winnerScreen
+        //TODO crear pantalla winnerScreen
     }
-    
+
     private void initGameOptions() {
-	//TODO crear pantalla gameOptionsScreen
+        //TODO crear pantalla gameOptionsScreen
     }
-    
+
     private void initSelectFile() {
-	//TODO crear pantalla selectFileScreen
+        //TODO crear pantalla selectFileScreen
     }
 }
