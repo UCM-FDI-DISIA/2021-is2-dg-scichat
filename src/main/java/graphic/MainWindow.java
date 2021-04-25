4642dffdf62ce1e1b08b1e242a6ec1a8a3282399
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2021-04-25 11:30:07 +0200

    Cambios en el formateo

--- src/main/java/graphic/MainWindow.java
+++ src/main/java/graphic/MainWindow.java
@@ -26,59 +26,56 @@ public class MainWindow extends JFrame implements GameObserver {
     private JPanel selectFileScreen = null;
 
     public MainWindow(Controller ctrl) {
-	super("Damas Chinas");
-	this.ctrl = ctrl;
-	ctrl.addObserver(this); // Necesitamos incluirlo como observador para que pueda darse cuenta de que la
-				// partida ha terminado
-	initGUI();
+        super("Damas Chinas");
+        this.ctrl = ctrl;
+        ctrl.addObserver(this); // Necesitamos incluirlo como observador para que pueda darse cuenta de que la
+        // partida ha terminado
+        initGUI();
     }
 
     private void initGUI() {
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	initStart();
-	this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initStart();
+        this.setVisible(true);
     }
 
     private void initStart() {
-	// No queremos que el juego anterior influya en el nuevo
-	if (gameScreen != null) {
-	    ctrl.reset();
-	    gameScreen = null;
-	}
-	if (startScreen == null)
-	    startScreen = new WelcomeWindow(this);
-	this.setContentPane(startScreen);
-	this.pack();
-	this.setSize(900, 700);
+        // No queremos que el juego anterior influya en el nuevo
+        if (gameScreen != null) {
+            ctrl.reset();
+            gameScreen = null;
+        }
+        if (startScreen == null) startScreen = new WelcomeWindow(this);
+        this.setContentPane(startScreen);
+        this.pack();
+        this.setSize(900, 700);
     }
 
     public void initGame() {
-	// TODO crear pantalla gameScreen
-	if (gameScreen == null) {
-
-	} else {
-	    ctrl.softReset();
-	}
-	this.setContentPane(gameScreen);
-	this.pack();
-	this.setSize(900, 700);
+        // TODO crear pantalla gameScreen
+        if (gameScreen == null) {} else {
+            ctrl.softReset();
+        }
+        this.setContentPane(gameScreen);
+        this.pack();
+        this.setSize(900, 700);
     }
 
     public void initWinner(Player winner) {
-	// TODO crear pantalla winnerScreen
+        // TODO crear pantalla winnerScreen
     }
 
     public void initGameOptions() {
-	// TODO crear pantalla gameOptionsScreen
-	System.out.println("Ahora se abriria el panel de opciones de partida");
+        // TODO crear pantalla gameOptionsScreen
+        System.out.println("Ahora se abriria el panel de opciones de partida");
     }
 
     public void initSelectFile() {
-	// TODO crear pantalla selectFileScreen
-	System.out.println("Ahora se abriria el panel de seleccionar partida");
+        // TODO crear pantalla selectFileScreen
+        System.out.println("Ahora se abriria el panel de seleccionar partida");
     }
 
     public void onGameEnded(Game game) {
-	// initWinner(game.getWinner());
+        // initWinner(game.getWinner());
     }
 }
