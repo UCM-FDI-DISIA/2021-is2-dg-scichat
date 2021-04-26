06bf46ded1b06bc3b2a0ca925afa5089ec83360a
Jimmy <jimmehcai@gmail.com>
2021-04-26 16:11:34 +0200

    Primera prueba para WebSocket
    Añadir dependencia a librería de JSON y WebSocket

---
+++ src/main/java/network/Client.java
@@ -0,0 +1,60 @@
+package network;
+
+import java.net.URI;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class Client extends WebSocketClient {
+
+    public Client(URI serverUri) {
+        super(serverUri);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake serverHandshake) {
+        System.out.println("Connected to server");
+    }
+
+    @Override
+    public void onMessage(String s) {
+        System.out.println("Received message: ");
+        JSONObject json = new JSONObject(new JSONTokener(s));
+        System.out.println(json);
+    }
+
+    @Override
+    public void onClose(int i, String s, boolean b) {
+        System.out.println("Disconnected");
+    }
+
+    @Override
+    public void onError(Exception e) {
+        System.out.println(e);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Client ws = new Client(new URI("ws://localhost:8080"));
+            ws.connect();
+
+            /// Cierra la conexión al salir del programa
+            Runtime
+                .getRuntime()
+                .addShutdownHook(
+                    new Thread(
+                        new Runnable() {
+
+                            public void run() {
+                                ws.close();
+                            }
+                        },
+                        "Shutdown-thread"
+                    )
+                );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
