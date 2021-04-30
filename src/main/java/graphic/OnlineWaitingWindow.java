d662cc0ae1fef474b103ac3137fe6a651f9ac5d4
Jimmy <jimmehcai@gmail.com>
2021-04-30 22:40:57 +0200

    Renderizar la informacion de room

--- src/main/java/graphic/OnlineWaitingWindow.java
+++ src/main/java/graphic/OnlineWaitingWindow.java
@@ -1,17 +1,25 @@
 package graphic;
 
 import java.awt.*;
+import java.util.Iterator;
+import java.util.Map;
 import javax.swing.*;
+import logic.Board;
+import logic.gameObjects.Piece;
 import network.client.SocketClient;
 import network.client.SocketObserver;
 import network.client.commands.Command;
 import network.client.commands.CommandParser;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import utils.Mode;
+import utils.PieceColor;
 
 public class OnlineWaitingWindow extends JFrame implements SocketObserver {
     private final SocketClient connection;
     private final String roomID;
-    private JEditorPane s = new JEditorPane();
+    private final JPanel roomInfoSection = new JPanel();
+    private JSONObject roomInfo;
 
     OnlineWaitingWindow(SocketClient _connection, String _roomID) {
         super("Habitación #" + _roomID);
@@ -44,15 +52,86 @@ public class OnlineWaitingWindow extends JFrame implements SocketObserver {
         mainContent.setBackground(Color.WHITE);
         mainContent.setLayout(new GridBagLayout());
 
-        JPanel container = new JPanel(new BorderLayout());
+        JPanel container = new JPanel(new BorderLayout(10, 20));
         mainContent.add(container);
 
+        JPanel topSection = new JPanel();
+        topSection.setLayout(new BoxLayout(topSection, BoxLayout.X_AXIS));
+        topSection.setBackground(Color.WHITE);
+
         JLabel roomIDLabel = new JLabel("Room ID: " + this.roomID);
+        topSection.add(roomIDLabel);
 
-        container.add(roomIDLabel, BorderLayout.PAGE_START);
-        container.add(s, BorderLayout.CENTER);
+        JPanel actionsSection = new JPanel();
+        actionsSection.setLayout(new BoxLayout(actionsSection, BoxLayout.X_AXIS));
 
+        JButton disconnectButton = new JButton("Desconectar");
+        disconnectButton.addActionListener(
+            e -> {
+                this.connection.close();
+                this.dispose();
+            }
+        );
+
+        JButton startGameButton = new JButton("Empezar el juego");
+        startGameButton.setEnabled(false);
+        actionsSection.add(startGameButton);
+
+        actionsSection.add(disconnectButton);
+
+        container.add(topSection, BorderLayout.NORTH);
+        container.add(roomInfoSection, BorderLayout.CENTER);
+        container.add(actionsSection, BorderLayout.PAGE_END);
+
+        this.pack();
         this.setVisible(true);
+    }
+
+    private void renderRoomInfo() {
+        if (this.roomInfo == null) return;
+
+        this.roomInfoSection.removeAll();
+        this.roomInfoSection.setLayout(new BorderLayout(10, 10));
+        this.roomInfoSection.setBackground(Color.WHITE);
+
+        int _modeIndex = this.roomInfo.getInt("mode");
+        int connectedPlayers = this.roomInfo.getInt("connectedPlayers");
+        JSONObject players = this.roomInfo.getJSONObject("players");
+
+        Mode mode = Mode.values()[_modeIndex];
+
+        JPanel topSection = new JPanel(new GridLayout(2, 1));
+        JLabel modeLabel = new JLabel("Modo del juego: " + mode);
+        JLabel connectedPlayersLabel = new JLabel(
+            "Jugadores conectados: " + connectedPlayers
+        );
+        topSection.add(modeLabel);
+        topSection.add(connectedPlayersLabel);
+
+        JPanel centerSection = new JPanel(new GridLayout(connectedPlayers, 1));
+
+        for (Iterator<String> it = players.keys(); it.hasNext();) {
+            String clientID = it.next();
+
+            JPanel playerSection = new JPanel(new FlowLayout());
+
+            JSONObject playerInfo = players.getJSONObject(clientID);
+            JLabel clientIDLabel = new JLabel("ID: " + clientID);
+
+            PieceColor color = PieceColor.values()[playerInfo.getInt("color")];
+            Board.Side side = Board.Side.values()[playerInfo.getInt("side")];
+
+            playerSection.add(clientIDLabel);
+            playerSection.add(new JLabel(color.toString()));
+            playerSection.add(new JLabel(side.toString()));
+
+            centerSection.add(playerSection);
+        }
+
+        roomInfoSection.add(topSection, BorderLayout.NORTH);
+        roomInfoSection.add(centerSection, BorderLayout.CENTER);
+
+        this.pack();
     }
 
     Command RoomInfoCommand = new Command("ROOM_INFO") {
@@ -60,7 +139,9 @@ public class OnlineWaitingWindow extends JFrame implements SocketObserver {
         @Override
         public void execute(JSONObject body, SocketClient connection) {
             /// Si recibe una información de la habitación, actualizar el GUI
-            s.setText(body.toString());
+            JSONObject data = body.getJSONObject("data");
+            roomInfo = data;
+            renderRoomInfo();
         }
     };
 
