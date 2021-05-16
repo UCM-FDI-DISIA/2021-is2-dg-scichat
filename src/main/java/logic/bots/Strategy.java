d6fb6fd0c02c63ce1377971f8367381516717c21
Ayhon <ayhonnick@gmail.com>
2021-05-16 12:13:00 +0200

    javadoc Strategy's move

--- src/main/java/logic/bots/Strategy.java
+++ src/main/java/logic/bots/Strategy.java
@@ -5,5 +5,14 @@ import logic.Cell;
 import logic.gameObjects.Player;
 
 public interface Strategy {
+    /**
+     * Dado un jugador y un tablero, selecciona la pieza a mover y devuelve
+     * la posición a la que moverla según la estrategia
+     *
+     * @param player jugador para el que calcular el movimiento
+     * @param jumpIsLimited decide si se permiten saltos de longitud mayor a 1
+     * @param board tablero en el que mover la pieza
+     * @return
+     */
     Cell move(Player player, boolean jumpIsLimited, Board board);
 }
