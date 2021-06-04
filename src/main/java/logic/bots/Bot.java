5aeb6ef6f90915160b0565a575ee8c855dc79933
Queco Jones <pamauro@ucm.es>
2021-06-04 20:21:46 +0200

    Un pequeño desliz y un primer intento de otro test

--- src/main/java/logic/bots/Bot.java
+++ src/main/java/logic/bots/Bot.java
@@ -27,11 +27,10 @@ public class Bot implements Player {
     private Board board;
     private Cell lastMovement = null; // Esto es para notificar a los observadores desde el Game
 
-    public Bot(Strategy strat, PieceColor color, Side start, String id, Side playerSide, String name, Board board, HashSet<Piece> pieces) {
+    public Bot(Strategy strat, PieceColor color, String id, Side playerSide, String name, Board board, HashSet<Piece> pieces) {
 	this.strategy = strat;
 	this.strategy = strat;
         this.color = color;
-        this.playerSide = start;
         this.id = id;
         this.playerSide = playerSide;
         this.name = name;
