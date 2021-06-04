7b6a6ecff1396f61d652a27afe537cf0e0961149
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2021-06-04 14:10:56 +0200

    Formateo que siempre se me pasa

--- src/main/java/control/LoadSaveCaretaker.java
+++ src/main/java/control/LoadSaveCaretaker.java
@@ -5,29 +5,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import logic.Game;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import logic.Game;
-
 public class LoadSaveCaretaker {
-    
     Game originator;
-    
 
-    
     public LoadSaveCaretaker(Game originator) {
-	this.originator = originator;
+        this.originator = originator;
     }
-    
+
     public void setOriginator(Game originator) {
-	this.originator = originator;
+        this.originator = originator;
     }
 
     public void saveGame(File file) {
-	JSONObject jGame = originator.toJSON();
-	
+        JSONObject jGame = originator.toJSON();
+
         try {
             FileWriter exit = new FileWriter(file);
             exit.append(jGame.toString());
@@ -38,16 +33,17 @@ public class LoadSaveCaretaker {
             System.out.println(ex.getMessage());
         }
     }
-    
+
     public Game loadGame(File file) {
         JSONObject jGame = null;
         try {
             jGame = new JSONObject(new JSONTokener(new FileInputStream(file)));
-        } catch (Exception ex) {} // Cambiar luego
-        
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
         originator = new Game(jGame);
-        
+
         return originator;
-        
     }
 }
